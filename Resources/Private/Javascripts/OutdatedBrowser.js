5147135b1fffc57fbe0d107855b9d7d08b9499ef
Jon Uhlmann <me@jonnitto.ch>
2017-11-08 09:35:42 +0100

    :hammer: Refactor package

--- Resources/Private/Javascripts/OutdatedBrowser.js
+++ Resources/Private/Javascripts/OutdatedBrowser.js
@@ -6,85 +6,93 @@ website:    http://www.burocratik.com
 * @preserve
 -----------------------------------------------------------------------*/
 (function() {
-	var outdated = document.getElementById('outdated');
-	var data = outdated.getAttribute('data-lowerthan');
-	var cssProp = data ? data : 'transform';
-	var hasCookie = false;
-	if (navigator.cookieEnabled) {
-		hasCookie = (function() {
-			var v = document.cookie.match('(^|;) ?outdatedbrowser=([^;]*)(;|$)');
-			return v ? v[2] == 'true' : null;
-		})();
-	}
+    var outdated = document.getElementById("outdated");
+    var data = outdated.getAttribute("data-lowerthan");
+    var cssProp = data ? data : "transform";
+    var hasCookie = false;
+    if (navigator.cookieEnabled) {
+        hasCookie = (function() {
+            var v = document.cookie.match(
+                "(^|;) ?outdatedbrowser=([^;]*)(;|$)"
+            );
+            return v ? v[2] == "true" : null;
+        })();
+    }
 
-	if (!hasCookie) {
-		//Define opacity and fadeIn/fadeOut functions
-		var done = true;
+    if (!hasCookie) {
+        //Define opacity and fadeIn/fadeOut functions
+        var done = true;
 
-		var opacity = function(opacityValue) {
-			outdated.style.opacity = opacityValue / 100;
-			outdated.style.filter = 'alpha(opacity=' + opacityValue + ')';
-		};
+        var opacity = function(opacityValue) {
+            outdated.style.opacity = opacityValue / 100;
+            outdated.style.filter = "alpha(opacity=" + opacityValue + ")";
+        };
 
-		var fadeIn = function(opacityValue) {
-			opacity(opacityValue);
-			if (opacityValue == 1) {
-				outdated.style.display = 'block';
-			}
-			if (opacityValue == 100) {
-				done = true;
-			}
-		};
+        var fadeIn = function(opacityValue) {
+            opacity(opacityValue);
+            if (opacityValue == 1) {
+                outdated.style.display = "block";
+            }
+            if (opacityValue == 100) {
+                done = true;
+            }
+        };
 
-		var supports = (function() {
-			var div = document.createElement('div');
-			var vendors = 'Khtml Ms O Moz Webkit'.split(' ');
-			var len = vendors.length;
+        var supports = (function() {
+            var div = document.createElement("div");
+            var vendors = "Khtml Ms O Moz Webkit".split(" ");
+            var len = vendors.length;
 
-			return function(prop) {
-				if (prop in div.style) {
-					return true;
-				}
+            return function(prop) {
+                if (prop in div.style) {
+                    return true;
+                }
 
-				prop = prop.replace(/^[a-z]/, function(val) {
-					return val.toUpperCase();
-				});
+                prop = prop.replace(/^[a-z]/, function(val) {
+                    return val.toUpperCase();
+                });
 
-				while (len--) {
-					if (vendors[len] + prop in div.style) {
-						return true;
-					}
-				}
-				return false;
-			};
-		})();
+                while (len--) {
+                    if (vendors[len] + prop in div.style) {
+                        return true;
+                    }
+                }
+                return false;
+            };
+        })();
 
-		//if browser does not supports css3 property (transform=default), if does > exit all this
-		if (!supports('' + cssProp + '')) { // jscs:ignore
-			if (done && outdated.style.opacity !== '1') {
-				done = false;
-				for (var i = 1; i <= 100; i++) {
-					// jshint -W083
-					setTimeout((function(x) {
-						return function() {
-							fadeIn(x);
-						};
-					})(i), i * 8);
-					// jshint +W083
-				}
-			}
-		} else {
-			return;
-		}
+        //if browser does not supports css3 property (transform=default), if does > exit all this
+        if (!supports("" + cssProp + "")) {
+            // jscs:ignore
+            if (done && outdated.style.opacity !== "1") {
+                done = false;
+                for (var i = 1; i <= 100; i++) {
+                    // jshint -W083
+                    setTimeout(
+                        (function(x) {
+                            return function() {
+                                fadeIn(x);
+                            };
+                        })(i),
+                        i * 8
+                    );
+                    // jshint +W083
+                }
+            }
+        } else {
+            return;
+        }
 
-		document.getElementById('btnCloseUpdateBrowser').onmousedown = function() {
-			outdated.style.display = 'none';
+        document.getElementById(
+            "btnCloseUpdateBrowser"
+        ).onmousedown = function() {
+            outdated.style.display = "none";
 
-			if (navigator.cookieEnabled) {
-				document.cookie = 'outdatedbrowser=true;path=/;';
-			}
+            if (navigator.cookieEnabled) {
+                document.cookie = "outdatedbrowser=true;path=/;";
+            }
 
-			return false;
-		};
-	}
+            return false;
+        };
+    }
 })();
