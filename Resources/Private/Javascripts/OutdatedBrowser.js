366a604df8a8871fb06c0db222c0c94efbb4ebd5
Jon Uhlmann <me@jonnitto.ch>
2018-05-28 18:46:59 +0200

    🏗 Switch from Cookie to LocalStorage

--- Resources/Private/Javascripts/OutdatedBrowser.js
+++ Resources/Private/Javascripts/OutdatedBrowser.js
@@ -2,77 +2,81 @@
 
 (function() {
     var outdated = document.getElementById("outdated");
-    var data = outdated.getAttribute("data-lowerthan");
-    var cssProp = data ? data : "transform";
-    var validBrowser = false;
-    var hasCookie = false;
-    if (navigator.cookieEnabled) {
-        hasCookie = (function() {
-            var v = document.cookie.match(
-                "(^|;) ?outdatedbrowser=([^;]*)(;|$)"
-            );
-            return v ? v[2] == "true" : null;
-        })();
-    }
-
-    if (!hasCookie) {
-        var supports = (function() {
-            var style = document.createElement("div").style;
-            var vendors = ["Moz", "O", "ms", "Webkit", "Khtml"];
-            var length = vendors.length;
-
-            return function(prop) {
-                if (prop in style) {
-                    return true;
-                }
-
-                prop = prop.replace(/(?:^|-)(\w)/g, function(matches, letter) {
-                    return letter.toUpperCase();
-                });
-
-                while (length--) {
-                    if (vendors[length] + prop in style) {
-                        return true;
-                    }
-                }
-                return false;
-            };
-        })();
-
-        var removeElement = function() {
-            outdated.parentNode.removeChild(outdated);
+    if (outdated) {
+        var data = outdated.getAttribute("data-lowerthan");
+        var cssProp = data ? data : "transform";
+        var validBrowser = false;
+        var storage = {
+            name: "outdatedbrowser",
+            value: "hide"
         };
 
-        // browser check by js props
-        if (/^js:+/g.test(cssProp)) {
-            var jsProp = cssProp.split(":")[1];
-            if (jsProp && jsProp == "Promise") {
-                validBrowser =
-                    window.Promise !== undefined &&
-                    window.Promise !== null &&
-                    Object.prototype.toString.call(window.Promise.resolve()) ===
-                        "[object Promise]";
-            }
-        } else {
-            validBrowser = supports("" + cssProp + "");
+        function removeElement() {
+            outdated.parentElement.removeChild(outdated);
         }
 
-        if (validBrowser) {
+        function hide() {
             removeElement();
-        } else {
-            outdated.style.display = "block";
+            localStorage.setItem(storage.name, storage.value);
+            return false;
+        }
 
-            document.getElementById(
-                "btnCloseUpdateBrowser"
-            ).onmousedown = function() {
+        function checkCssProp() {
+            if (localStorage.getItem(storage.name) == storage.value) {
                 removeElement();
+            } else {
+                var supports = (function() {
+                    var style = document.createElement("div").style;
+                    var vendors = ["Moz", "O", "ms", "Webkit", "Khtml"];
+                    var length = vendors.length;
 
-                if (navigator.cookieEnabled) {
-                    document.cookie = "outdatedbrowser=true;path=/;";
+                    return function(prop) {
+                        if (prop in style) {
+                            return true;
+                        }
+
+                        prop = prop.replace(/(?:^|-)(\w)/g, function(
+                            matches,
+                            letter
+                        ) {
+                            return letter.toUpperCase();
+                        });
+
+                        while (length--) {
+                            if (vendors[length] + prop in style) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    };
+                })();
+
+                // browser check by js props
+                if (/^js:+/g.test(cssProp)) {
+                    var jsProp = cssProp.split(":")[1];
+                    if (jsProp && jsProp == "Promise") {
+                        validBrowser =
+                            window.Promise !== undefined &&
+                            window.Promise !== null &&
+                            Object.prototype.toString.call(
+                                window.Promise.resolve()
+                            ) === "[object Promise]";
+                    }
+                } else {
+                    validBrowser = supports("" + cssProp + "");
                 }
 
-                return false;
-            };
+                if (validBrowser) {
+                    removeElement();
+                } else {
+                    outdated.style.display = "block";
+                    document.getElementById(
+                        "btnCloseUpdateBrowser"
+                    ).onmousedown = hide;
+                }
+            }
         }
+
+        checkCssProp();
     }
 })();
